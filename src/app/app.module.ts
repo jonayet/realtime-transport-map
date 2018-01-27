027c9dd1e7a787510fb361ddf6dc73df0f80ef9b
Jonayet Hossain <jonayet.hossain@etventure.com>
2018-01-27 12:21:27 +0100

    service worker added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { TransportMapComponent } from './transport-map/transport-map.component';
 
@@ -12,7 +14,8 @@ import { TransportMapComponent } from './transport-map/transport-map.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
